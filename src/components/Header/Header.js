19db03cb465ac55a9efcc09bfac83d2f4d95fe4d
damian-balas <damian.webdev@gmail.com>
2019-10-28 12:56:47 +0100

    Integrate firebase into application, create sign in page with sign in component and sign in with Google

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 
 import { ReactComponent as Logo } from '../../assets/4.4 crown.svg.svg'
 import './Header.sass'
+import { auth } from '../../firebase/firebaseUtils'
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <header className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo' />
@@ -12,6 +13,12 @@ const Header = () => (
     <nav className='options'>
       <Link to='/shop' className='option'>SHOP</Link>
       <Link to='/shop' className='option'>CONTACT</Link>
+      {
+        currentUser ? 
+        <button className='option sign-out' onClick={() => auth.signOut()}>SIGN OUT</button>
+        : 
+        <Link className='option' to='/signin'>SIGN IN</Link>
+      }
     </nav>
   </header>
 )
---
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/SignIn/SignIn'
+
+import './SignInAndSignUp.sass'
+
+const SignInAndSignUp = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUp;
