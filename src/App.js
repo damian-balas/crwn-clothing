ac7ea7dee7823a15d892bfd468f50937be93ed15
damian-balas <damian.webdev@gmail.com>
2019-11-05 15:12:37 +0100

    Add redux, implenet userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux'
 
 import './App.css';
 
@@ -8,33 +9,25 @@ import Shop from './pages/Shop/Shop';
 import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp';
 import Header from './components/Header/Header';
 import { auth, createUserProfileDocument } from './firebase/firebaseUtils';
-
+import { setCurrentUser } from './redux/user/user.actions'
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unsubscribeFromAuth = null;
-
+  
   componentDidMount() {
+    const {setCurrentUser} = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          
+            setCurrentUser({
               id: snapShot.id,
               ...snapShot.data(),
-            }
-          })
+            })
         })
       } else {
-        this.setState({ currentUser: userAuth})
+        setCurrentUser(userAuth)
       }
     });
   }
@@ -46,7 +39,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={Shop} />
@@ -57,4 +50,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
